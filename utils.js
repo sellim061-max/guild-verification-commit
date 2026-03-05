ab46ccde241d03236da42a3af2b16b72b70fff85
sellim061-max <sellim061@gmail.com>
2026-03-05 15:02:38 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:02:32.130Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T12:02:38.987Z
+Commit: Fix CSS styling bug on mobile devices
