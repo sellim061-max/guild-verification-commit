232be95ba104df7b0d6f416b1397ec7d84ee16bc
sellim061-max <sellim061@gmail.com>
2026-03-05 15:02:50 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:02:38.987Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T12:02:51.191Z
+Commit: Fix broken links in documentation
