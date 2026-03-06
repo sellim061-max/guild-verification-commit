693524eb05495a7754771b239e7f701f7ac37713
sellim061-max <sellim061@gmail.com>
2026-03-06 13:01:03 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:03:28.597Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T10:01:04.932Z
+Commit: Fix broken links in documentation
